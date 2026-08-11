dcc8bb8837d934b8dd38d4c9c81f9d14cf6edaf5
YodZ0 <timur.g00gle@gmail.com>
2026-08-12 02:40:28 +0500

    feat(api): include healthz router

--- src/api/v1.py
+++ src/api/v1.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from src.settings import settings
 
+from src.apps.healthz.router import router as healthz_router
+from src.settings import settings
 
 router_v1 = APIRouter(prefix=settings.api.v1.prefix)
 # Include API routers
-# router_v1.include_router()
+router_v1.include_router(healthz_router)
