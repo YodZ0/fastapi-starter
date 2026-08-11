6a7fe78705cc162d36e8f48a2eb53979685c14dc
YodZ0 <timur.g00gle@gmail.com>
2026-08-12 02:39:48 +0500

    feat(core): update base exceptions

--- src/core/exceptions/__init__.py
+++ src/core/exceptions/__init__.py
@@ -1,3 +1,8 @@
-from .repository import ModelNotFoundError as ModelNotFoundError
+"""
+Base exceptions module.
+"""
+
+from .business_error import BusinessLogicException as BusinessLogicException
 from .repository import ModelIntegrityError as ModelIntegrityError
-from .repository import ModelAlreadyExistsError as ModelAlreadyExistsError
+from .repository import ModelNotFoundError as ModelNotFoundError
+from .repository import SortingFieldNotFoundError as SortingFieldNotFoundError
---
+++ src/core/exceptions/business_error.py
@@ -0,0 +1,44 @@
+import traceback
+from abc import ABC, abstractmethod
+
+from pydantic.alias_generators import to_snake
+
+from src.core.schemas import BusinessLogicExceptionSchema
+
+__all__ = ("BusinessLogicException",)
+
+
+class BusinessLogicException(Exception, ABC):
+    @property
+    def type(self) -> str:
+        """
+        Error type.
+        """
+        return to_snake(type(self).__name__.replace("Error", ""))
+
+    @property
+    @abstractmethod
+    def msg(self) -> str:
+        """
+        Error message.
+        """
+        ...
+
+    def __str__(self) -> str:
+        return self.msg
+
+    def get_schema(self, debug: bool) -> BusinessLogicExceptionSchema:
+        """
+        Get exception schema.
+        """
+        return BusinessLogicExceptionSchema(
+            type=self.type,
+            msg=self.msg,
+            traceback=(
+                "".join(
+                    traceback.format_exception(type(self), self, self.__traceback__)
+                )
+                if debug
+                else None
+            ),
+        )
